b1623abf1b88bbb86d15124c88b035df5f300a61
ONEWateR <bawgame1993@gmail.com>
2014-05-17 13:34:36 +0800

    MaximumDepthOfBinaryTree

---
+++ MaximumDepthOfBinaryTree.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+        	return 0;
+        
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+        
+        return 1 + Math.max(l, r);
+    }
+}
